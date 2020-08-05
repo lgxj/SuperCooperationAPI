0d34a15221cc178f5f911dc0b9e32098250872d1
lgxj <68359028+lgxj@users.noreply.github.com>
2020-08-05 14:27:52 +0800

    Update oss.php

--- config/oss.php
+++ config/oss.php
@@ -1,8 +1,8 @@
 <?php
 return [
     'aliyun'=>[
-        'access_key_id'=>'LTAI4FnDXgLjGBfKYWNbGug4',
-        'access_key_secret'=> 'VapUtQi6oRmLnSk1Is29orQkA2y2Mv',
+        'access_key_id'=>'LTAI4FnDXgLjGBfwqeqwKYWNbGug4', // 修改为自己的
+        'access_key_secret'=> 'VapUtQi6oRmLnSk1Iqweqweqws29orQkA2y2Mv', // 修改为自己的
         'endpoint'=>'oss-cn-qingdao.aliyuncs.com',
         'bucket' => 'bbimgs'
     ]
